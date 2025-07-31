8a87867e88d8c5d001bf1019b44e7b2152b4f4ac
0xgouda <gouda0x@gmail.com>
2025-07-31 22:21:50 +0300

    [+] add steps tracing print statements

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ const (
 )
 
 func SendQuery(queryPacket []byte, serverAddr string) DNSResponse {
+	fmt.Println("Querying:", serverAddr)
+
 	// For simplicity lets hope it will make it to the server and back :)
 	socket, _ := net.Dial("udp", serverAddr + ":53")
 	defer socket.Close()
-
 	socket.Write(queryPacket)
 
 	buf := make([]byte, 1024)
@@ -25,12 +26,22 @@ func SendQuery(queryPacket []byte, serverAddr string) DNSResponse {
 	if resp.Header.ANcount > 0 {
 		return resp
 	} 
+
+	if serverAddr == A_ROOT_SERVER {
+		fmt.Printf("Received TLD Server Address for \"%s\": \"%s\"", resp.NameServers[0].DomainName, resp.NameServers[0].RData)
+	} else {
+		fmt.Printf("Received Authoritative Server Address for \"%s\": \"%s\"", resp.NameServers[0].DomainName, resp.NameServers[0].RData)
+	}
+	fmt.Println()
 	
 	var nsIp string
 	var nsAns []DNSRecord
 	if resp.Header.ARcount > 0  {
 		nsAns = resp.AdditionalRecords
 	} else {
+		fmt.Println()
+		fmt.Println("IP not in packet, starting new query for:", resp.NameServers[0].RData)
+
 		nsQuery := GenerateDNSQuery(resp.NameServers[0].RData)
 		nsAns = SendQuery(nsQuery, A_ROOT_SERVER).Answers
 	}
@@ -41,12 +52,22 @@ func SendQuery(queryPacket []byte, serverAddr string) DNSResponse {
 		}
 	}
 
+	fmt.Println("Resolved IP:", nsIp)
+	if resp.Header.ARcount == 0 {
+		fmt.Println("new query done")
+	}
+	fmt.Println()
+
 	return SendQuery(queryPacket, nsIp)
 }
 
 func main() {
+	fmt.Println("Fixed Root Server IP:", A_ROOT_SERVER)
+	fmt.Println()
+
 	queryPakcet := GenerateDNSQuery(os.Args[1])
 	resp := SendQuery(queryPakcet, A_ROOT_SERVER)
+	fmt.Println("Received:")
 
 	for _, ans := range resp.Answers {
 		fmt.Println("Domain:", ans.DomainName, "IP:", ans.RData)

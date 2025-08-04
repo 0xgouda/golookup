cc04bb422be082e9411d4d4b1290c55f41582717
0xgouda <gouda0x@gmail.com>
2025-08-04 12:08:41 +0300

    [*] move recursive query resolving logic to `conn.go`
    add separate `Resolve()` that recursively uses `SendDNSQuery()`

---
+++ conn.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+// Fixed Root DNS Servers addresses
+// From: https://www.iana.org/domains/root/servers
+const (
+	A_ROOT_SERVER = "198.41.0.4"
+)
+
+func Resolve(domain string, qtype RecordType) (*DNSResponse, error) {
+	queryPacket := GenerateDNSQuery(domain, qtype)
+	serverToQuery := A_ROOT_SERVER
+
+	for {
+		fmt.Println("Querying:", serverToQuery)
+		buf, err := SendDNSQuery(queryPacket, serverToQuery)
+		if err != nil {
+			return nil, err
+		}
+
+		resp := ParseDNSResponse(buf)
+		if resp.Header.ANcount > 0 {
+			return resp, nil
+		}
+
+		ns, nsIp := GetNameServerToQuery(resp)
+		if len(resp.NameServers) == 0 || ns == "" {
+			return nil, fmt.Errorf("no answer or name servers records found in packet")
+		}
+
+		if serverToQuery == A_ROOT_SERVER {
+			fmt.Printf("Received TLD Server Address for \"%s\": \"%s\"", domain, ns)
+		} else {
+			fmt.Printf("Received Authoritative Server Address for \"%s\": \"%s\"", domain, ns)
+		}
+		fmt.Println()
+		
+		if nsIp == "" {
+			fmt.Println()
+			fmt.Println("IP not in packet, starting new query for:", ns)
+
+			nsResp, err := Resolve(ns, A_TYPE)
+			if err != nil {
+				return nil, err
+			}
+			nsIp = nsResp.Answers[0].RData
+			fmt.Println("new query done")
+			fmt.Println()
+		}
+		
+		serverToQuery = nsIp
+		fmt.Println("Resolved IP:", nsIp)
+		fmt.Println()
+	}
+}
+
+func SendDNSQuery(queryPacket []byte, serverAddr string) ([]byte, error) {
+	socket, _ := net.Dial("udp", serverAddr + ":53")
+	defer socket.Close()
+
+	buf := make([]byte, 1024)
+	var err error
+	for range 5 {
+		socket.SetDeadline(time.Now().Add(5 * time.Second))
+		_, err = socket.Write(queryPacket)
+		if err == nil {
+			_, err = socket.Read(buf)
+			if err == nil {
+				break
+			}
+		}
+	}
+
+	if err != nil {
+		if errors.Is(err, os.ErrDeadlineExceeded) {
+			return nil, fmt.Errorf("error connecting to address %s, UDP packets didn't make it", serverAddr)
+		}
+		return nil, err
+	} 
+
+	return buf, nil
+}
+
+func GetNameServerToQuery(resp *DNSResponse) (string, string) {
+	for _, ar := range resp.AdditionalRecords {
+		if ar.Type_ == A_TYPE {
+			return ar.DomainName, ar.RData
+		}
+	}
+
+	for _, nsRecord := range resp.NameServers {
+		if nsRecord.RData != "" {
+			return nsRecord.RData, ""
+		}
+	}	
+
+	return "", ""
+}
--- main.go
+++ main.go
@@ -1,98 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net"
 	"os"
 	"strings"
-	"time"
 )
-
-// Fixed Root DNS Servers addresses
-// From: https://www.iana.org/domains/root/servers
-const (
-	A_ROOT_SERVER = "198.41.0.4"
-)
-
-func SendDNSQuery(queryPacket []byte, serverAddr string) (*DNSResponse, error) {
-	fmt.Println("Querying:", serverAddr)
-
-	socket, _ := net.Dial("udp", serverAddr + ":53")
-	defer socket.Close()
-
-	buf := make([]byte, 1024)
-	var err error
-	for range 5 {
-		socket.SetDeadline(time.Now().Add(5 * time.Second))
-		_, err = socket.Write(queryPacket)
-		if err == nil {
-			_, err = socket.Read(buf)
-			if err == nil {
-				break
-			}
-		}
-	}
-
-	if errors.Is(err, os.ErrDeadlineExceeded) {
-		return nil, fmt.Errorf("error connecting to address %s, UDP packets didn't make it", serverAddr)
-	}
-
-	resp := ParseDNSResponse(buf)
-	if resp.Header.ANcount > 0 {
-		return resp, nil
-	} 
-
-	if serverAddr == A_ROOT_SERVER {
-		fmt.Printf("Received TLD Server Address for \"%s\": \"%s\"", resp.NameServers[0].DomainName, resp.NameServers[0].RData)
-	} else if resp.NameServers[0].RData != "" {
-		fmt.Printf("Received Authoritative Server Address for \"%s\": \"%s\"", resp.NameServers[0].DomainName, resp.NameServers[0].RData)
-	} else {
-		return &DNSResponse{}, nil
-	}
-	fmt.Println()
-	
-	var nsIp string
-	var nsAns []DNSRecord
-	if resp.Header.ARcount > 0  {
-		nsAns = resp.AdditionalRecords
-	} else {
-		fmt.Println()
-		fmt.Println("IP not in packet, starting new query for:", resp.NameServers[0].RData)
-
-		nsQuery := GenerateDNSQuery(resp.NameServers[0].RData, A_TYPE)
-		nsResp, err := SendDNSQuery(nsQuery, A_ROOT_SERVER)
-		if err != nil {
-			return nil, err
-		}
-		nsAns = nsResp.Answers
-	}
-	
-	for _, ans := range nsAns {
-		if ans.Type_ == A_TYPE {
-			nsIp = ans.RData
-			break
-		}
-	}
-
-	fmt.Println("Resolved IP:", nsIp)
-	if resp.Header.ARcount == 0 {
-		fmt.Println("new query done")
-	}
-	fmt.Println()
-
-	return SendDNSQuery(queryPacket, nsIp)
-}
 
 func main() {
-	if len(os.Args) < 3 {
+	args := os.Args[1:]
+	if len(args) < 2 {
 		fmt.Println("usage: golookup [domain] [A|NS|CNAME|MX|TXT]")
 		return 
 	}
 
 	var qtype RecordType
 	var resultName string
-	switch strings.ToUpper(os.Args[2]) {
+	switch strings.ToUpper(args[1]) {
 	case "A":
 		qtype = A_TYPE
 		resultName = "IP"
@@ -116,16 +39,14 @@ func main() {
 	fmt.Println("Fixed Root Server IP:", A_ROOT_SERVER)
 	fmt.Println()
 
-	queryPakcet := GenerateDNSQuery(os.Args[1], qtype)
-	resp, err := SendDNSQuery(queryPakcet, A_ROOT_SERVER)
+	resp, err := Resolve(args[0], qtype)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("Received:")
 
+	fmt.Println("Answers:")
 	for _, ans := range resp.Answers {
-		fmt.Println("Name:", ans.DomainName)
 		fmt.Println(resultName + ":", ans.RData)
 	}
 }

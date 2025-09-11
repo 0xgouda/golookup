7154c2db1357639e5a6d1bdb4a0e8d76d57e2a77
0xgouda <gouda0x@gmail.com>
2025-09-11 06:17:42 +0300

    [+] implement `serveDNS(port)` which starts a DNS server on the given port
    - update `Resolve()` to use new `DNSPacket` type
    - fix `GetNameServer()` logic

--- conn.go
+++ conn.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net"
-	"os"
 	"time"
 )
 
@@ -14,93 +12,124 @@ const (
 	A_ROOT_SERVER = "198.41.0.4"
 )
 
-func Resolve(query DNSQuery) (*DNSResponse, error) {
+// Iterates through the DNS hierarchy to get query answer
+func Resolve(query DNSPacket) (DNSPacket, error) {
 	serverToQuery := A_ROOT_SERVER
 	for {
-		fmt.Println("Querying:", serverToQuery)
 		buf, err := SendDNSQuery(query, serverToQuery)
 		if err != nil {
-			return nil, err
+			return DNSPacket{}, err
 		}
 
-		resp := ParseDNSResponse(buf)
+		resp := ParseDNSPacket(buf)
 		if resp.Header.ANcount > 0 {
-			// answer found, return.
+			// answer found
 			return resp, nil
 		}
 
-		ns, nsIp := GetNameServerToQuery(resp)
-		if len(resp.NameServers) == 0 || ns == "" {
-			return nil, fmt.Errorf("no answer or name servers records found in packet")
+		if resp.Header.NScount == 0 {
+			return DNSPacket{}, fmt.Errorf("no answer or name server records found in packet")
 		}
+		ns, nsIp := GetNameServer(resp)
 
-		if serverToQuery == A_ROOT_SERVER {
-			fmt.Printf("Received TLD Server Address for \"%s\": \"%s\"", query.Questions[0].Qname, ns)
-		} else {
-			fmt.Printf("Received Authoritative Server Address for \"%s\": \"%s\"", query.Questions[0].Qname, ns)
-		}
-		fmt.Println()
-		
 		if nsIp == "" {
-			fmt.Println()
-			fmt.Println("name server IP not in packet")
+			fmt.Println("name server IP not found in packet")
 			fmt.Println("starting new query for:", ns)
-
 			newQuery := GenerateDNSQuery(ns, A_TYPE)
 			nsResp, err := Resolve(newQuery)
 			if err != nil {
-				return nil, err
+				return DNSPacket{}, err
 			}
 			nsIp = nsResp.Answers[0].RData
-			fmt.Println("new query done, found name server IP")
-			fmt.Println()
+			fmt.Printf("new query done, found \"%s\" IP: \"%s\"", ns, nsIp)
 		}
-		
 		serverToQuery = nsIp
-		fmt.Println("Resolved IP:", nsIp)
-		fmt.Println()
 	}
 }
 
-func SendDNSQuery(query DNSQuery, serverAddr string) ([]byte, error) {
-	socket, _ := net.Dial("udp", serverAddr + ":53")
-	defer socket.Close()
+// Sends the DNS query to server address over UDP with timeout and retrial
+func SendDNSQuery(query DNSPacket, serverAddr string) ([]byte, error) {
+	fmt.Println("Querying:", serverAddr)
+	conn, _ := net.Dial("udp", serverAddr + ":53")
+	defer conn.Close()
 
 	buf := make([]byte, 1024)
 	var err error
 	for range 5 {
-		socket.SetDeadline(time.Now().Add(5 * time.Second))
-		_, err = socket.Write(query.ToBytes())
+		conn.SetDeadline(time.Now().Add(5 * time.Second))
+		_, err = conn.Write(query.ToBytes())
 		if err == nil {
-			_, err = socket.Read(buf)
+			_, err = conn.Read(buf)
 			if err == nil {
 				break
 			}
 		}
 	}
-
-	if err != nil {
-		if errors.Is(err, os.ErrDeadlineExceeded) {
-			return nil, fmt.Errorf("error connecting to address %s, UDP packets didn't make it", serverAddr)
-		}
-		return nil, err
-	} 
-
-	return buf, nil
+	return buf, err
 }
 
-func GetNameServerToQuery(resp *DNSResponse) (string, string) {
-	for _, ar := range resp.AdditionalRecords {
-		if ar.Type == A_TYPE {
-			return ar.DomainName, ar.RData
+// Gets domain name and ip for a name server record from 
+// the given dns response packet.
+//
+// If any field is not found it returns its zero value.
+func GetNameServer(resp DNSPacket) (string, string) {
+	for i, nsr := range resp.NameServers {
+		for _, ar := range resp.AdditionalRecords {
+			if ar.Type == A_TYPE && ar.DomainName == nsr.RData {
+				fmt.Printf("Received Name Server for \"%s\": \"%s\"\n", nsr.DomainName, nsr.RData)
+				fmt.Printf("Received \"%s\" IP: \"%s\"\n", ar.DomainName, ar.RData)
+				return nsr.RData, ar.RData
+			}
 		}
-	}
 
-	for _, nsRecord := range resp.NameServers {
-		if nsRecord.RData != "" {
-			return nsRecord.RData, ""
+		if i + 1 == len(resp.NameServers) {
+			fmt.Printf("Received Name Server for \"%s\": \"%s\"\n", nsr.DomainName, nsr.RData)
+			return nsr.RData, ""
 		}
 	}	
-
 	return "", ""
+}
+
+// Starts a DNS server listening at the given port
+func serveDNS(port int) error {
+	addr := net.UDPAddr{
+        Port: port,
+        IP:   net.ParseIP("0.0.0.0"),
+    }
+    conn, err := net.ListenUDP("udp", &addr)
+    if err != nil {
+        return err
+    }
+	conn.SetReadDeadline(time.Time{}) // No Read Deadline
+    defer conn.Close()
+
+    for {
+		buffer := make([]byte, 1024)
+        _, addr, err := conn.ReadFromUDP(buffer)
+        if err != nil {
+            fmt.Println(err)
+            continue
+        }
+
+		query := ParseDNSPacket(buffer)
+		newQuery := GenerateDNSQuery(query.Questions[0].Qname, query.Questions[0].Qtype)
+		resp, err := Resolve(newQuery)
+		if err != nil {
+            fmt.Println(err)
+			continue
+		}
+		resp.Header.Id = query.Header.Id
+		resp.Header.Flags = 1 << 15
+
+		for range 5 {
+			conn.SetWriteDeadline(time.Now().Add(5 * time.Second))
+			_, err = conn.WriteToUDP(resp.ToBytes(), addr)
+			if err == nil {
+				break
+			}
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+    }
 }
